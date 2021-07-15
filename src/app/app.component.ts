167206a9285cd045e3973db7ae92f694577766f3
Claudiu Gombos <glo51506@adobe.com>
2021-07-15 22:46:30 +0300

    email editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,78 @@ export class AppComponent implements OnInit {
   htmlText ="<p>Your message</p>"
   title = 'conditionMongoQuery';
 
+  quillConfig={
+    //toolbar: '.toolbar',
+    toolbar: {
+      container: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['code-block'],
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        //[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        //[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        //[{ 'direction': 'rtl' }],                         // text direction
+
+        //[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        //[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+        //[{ 'font': [] }],
+        //[{ 'align': [] }],
+
+        ['clean'],                                         // remove formatting button
+
+        ['link'],
+        //['link', 'image', 'video']  
+      ],
+      
+    },
+
+    mention: {
+      allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+      mentionDenotationChars: ["@", "#"],
+      source: (searchTerm, renderList, mentionChar) => {
+        let values;
+
+        // if (mentionChar === "@") {
+        //   values = this.atValues;
+        // } else {
+        //   values = this.hashValues;
+        // }
+        
+        if (searchTerm.length === 0) {
+          renderList(values, searchTerm);
+        } else {
+          const matches = [];
+          for (var i = 0; i < values.length; i++)
+            if (~values[i].value.toLowerCase().indexOf(searchTerm.toLowerCase())) matches.push(values[i]);
+          renderList(matches, searchTerm);
+        }
+      },
+    },
+    "emoji-toolbar": true,
+    "emoji-textarea": false,
+    "emoji-shortname": true,
+    keyboard: {
+      bindings: {
+        // shiftEnter: {
+        //   key: 13,
+        //   shiftKey: true,
+        //   handler: (range, context) => {
+        //     // Handle shift+enter
+        //     console.log("shift+enter")
+        //   }
+        // },
+        enter:{
+          key:13,
+          handler: (range, context)=>{
+            console.log("enter");
+            return true;
+          }
+        }
+      }
+    }
+  }
+
   ngOnInit(): void {
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/general/home/home.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { QuillModule } from 'ngx-quill'
 @NgModule({
   declarations: [
@@ -17,6 +17,7 @@ import { QuillModule } from 'ngx-quill'
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     QuillModule.forRoot()
   ],
   providers: [],
